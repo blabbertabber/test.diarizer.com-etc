7510ef22af673946fb7d1f8e8358d84fc7e47923
Brian Cunnie <brian.cunnie@gmail.com>
2017-05-12 17:33:01 -0700

    Move to hostname-neutral filenames
    - makes for consistent layout for diarizer.com, test.diarizer.com

--- cron.weekly/letsencrypt.sh
+++ cron.weekly/letsencrypt.sh
@@ -2,9 +2,9 @@
 set -eux
 sudo -u acme_tiny python /usr/local/bin/acme_tiny.py \
     --account-key /etc/pki/letsencrypt.key \
-    --csr /etc/pki/nginx/diarizer.blabbertabber.com.csr \
+    --csr /etc/pki/nginx/server.csr \
     --acme-dir /var/blabbertabber/acme-challenge > /tmp/signed.crt || exit
 wget -O- https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem > /tmp/intermediate.pem
 cat /tmp/signed.crt /tmp/intermediate.pem |
-    sudo -u acme_tiny tee /etc/pki/nginx/diarizer.blabbertabber.com.crt
+    sudo -u acme_tiny tee /etc/pki/nginx/server.crt
 sudo systemctl restart nginx.service
